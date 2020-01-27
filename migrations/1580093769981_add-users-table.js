83425d0b839f277ea797559199c99428b150b90c
jakembalbedina <jakembalbedina@gmail.com>
2020-01-27 11:31:51 +0800

    migration on user

--- migrations/1580093769981_add-users-table.js
+++ migrations/1580093769981_add-users-table.js
@@ -2,6 +2,31 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = pgm => {
+    pgm.createTable('users', {
+        user_id: {
+            type: 'integer',
+            primaryKey: true,
+        },
+        username: {
+            type: 'text',
+            notNull: true,
+        },
+        password: {
+            type: 'text',
+            notNull: true,
+        },
+        user_role_id: {
+            type: 'integer',
+            notNull: true,
+            references: '"user_role"',
+        },
+        user_info_id: {
+            type: 'integer',
+            notNull: true,
+            references: '"user_info"',
+        },
+    });
+};
 
-exports.down = pgm => {};
+exports.down = pgm => { };
